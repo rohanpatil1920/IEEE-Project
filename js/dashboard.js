20eb7a1ebf46d7301576bbdf6dd808c9eef157de
madhusudan babar <madhusudanbabar@gmail.com>
2020-04-10 12:20:30 +0530

    redesigned input table and updated databse code

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,11 +1,39 @@
 
+class db{
+  constructor(farmer, produce, quantity, cost, type){
+    this.farmer = farmer;
+    this.produce = produce;
+    this.quantity = quantity;
+    this.cost = cost;
+    this.type = type;
+  }
+}
+
 function submitData() {
- // var name = document.getElementById("").value;
-  var contact = document.getElementById('contact').value;
+  var farmer = document.getElementById('name_of_farmer').value.trim();
+  var produce = document.getElementById('produce').value.trim();
+  var quantity = document.getElementById('quantity').value.trim();
+  var cost = document.getElementById('cost_of_cultivation').value.trim();
+  var type = document.getElementById('type').value.trim();
+  var user = firebase.auth().currentUser;
+  console.log(user);
+  console.log(farmer, produce, quantity, cost );
 
-  console.log(contact);
+  var details = new db(farmer, produce, quantity, cost, type);
+  console.log(details);
 
-  firebase.database().ref().push("hello world");
-  // window.alert("hi")
-
+    if (farmer !== "" && produce !== "" && quantity !== "" && cost !== "" && type !== "") {
+      console.log(farmer);
+      if (user) {
+        firebase.database().ref().child(user.displayName).push(details);
+        document.getElementById('success').style.display = "block";
+      }
+      else {
+        alert("An unexpected error occured!\nplease login again to continue");
+        window.location = "register.html";
+      }
+    }
+    else {
+      document.getElementById('error').style.display = "block";
+    }
 }
